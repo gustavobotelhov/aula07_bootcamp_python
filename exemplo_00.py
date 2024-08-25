ca0ce3146ca9edf1f992924235093ddeb4baef54
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-25 16:50:20 -0300

    adicionando etl de csv

---
+++ exemplo_00.py
@@ -0,0 +1,9 @@
+valor_1 = 3
+valor_2 = 4
+
+def somar(valor_1_para_soma: float, valor_2_para_somar: float) -> float:
+    resultado_da_soma = valor_1_para_soma + valor_2_para_somar
+    return resultado_da_soma
+
+print(somar(valor_1,valor_2))
+
